bf6d58ec6ff7010cf938e5fb606c534b84793086
Frederick <frederick.gfl@gmail.com>
2019-12-18 03:35:29 +0000

    add icons to tweets

--- public/scripts/update-tweets.js
+++ public/scripts/update-tweets.js
@@ -1,27 +1,37 @@
 /**
  * Dec 17, 2019
  * Frederick Lee
+ * 
+ * icons Font Awesome. License: https://fontawesome.com/license
  */
 
 $(document).ready(function() {
   const tweets = $.get('/tweets', function(data, status) {
     if (status === 'success') {
       for (tweet of data) {
+        const avatar = tweet.user.avatars;
+        const name = tweet.user.name;
+        const handle = tweet.user.handle;
+        const date = new Date(tweet.created_at).toDateString();
+        const text = tweet.content.text;
+
+
         $('main').append(`
           <article class="section tweet">
             <header>
               <div>
-                <img src=${tweet.user.avatars}><h3 class="name">${tweet.user.name}</h3>
+                <img src="${avatar}"><h3 class="name">${name}</h3>
               </div>
-              <h3 class="handle">${tweet.user.handle}</h3>
+              <h3 class="handle">${handle}</h3>
             </header>
-            <p class="msg">${tweet.content.text}></p>
+            <p class="msg">${text}></p>
             <footer>
-              <p>${tweet.created_at}</p>
+              <p>${date}</p>
               <div>
-
+                <img class="icon" src="/images/flag-solid.svg" />
+                <img class="icon" src="/images/retweet-solid.svg" />
+                <img class="icon" src="/images/heart-solid.svg" />
               </div>
-              <img/>
             </footer>
               
           </article>
@@ -29,6 +39,5 @@ $(document).ready(function() {
 
       }
     }
-    console.log('data',data);
   });
 });
